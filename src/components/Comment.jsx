2011c11708b9638fc2bc7945a5225c3a3e98c6d0
Brijen Makwana <brijenmakwana@192.168.1.7>
2023-06-06 10:38:48 +0530

    Comment styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import { Flex, Tag, Text } from "@chakra-ui/react";
 const Comment = (props) => {
   const { batchYear, comment } = props;
   return (
-    <Flex padding="5px 0" alignItems="center">
+    <Flex padding="5px 0" alignItems="center" justifyContent="space-between">
       <Tag size="md" variant="subtle" marginTop="5px">
         {batchYear}
       </Tag>
