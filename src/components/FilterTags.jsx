69cafdac426edaa2b9f228a206145a614b45c1f8
Brijen Makwana <brijenmakwana@192.168.1.6>
2023-06-08 10:29:14 +0530

    Accordion in FilterBar

--- src/components/FilterTags.jsx
+++ src/components/FilterTags.jsx
@@ -1,37 +1,31 @@
 import { Box, Heading, Tag, TagLabel, TagCloseButton } from "@chakra-ui/react";
 
 export default function FilterTags(props) {
-  const { heading, tags, selectedTags, handleSelection } = props;
+  const { tags, selectedTags, handleSelection } = props;
 
   return (
-    <Box marginBottom="15px">
-      <Heading as="h3" textTransform="capitalize" fontSize="md" color="#000">
-        {heading}
-      </Heading>
+    <>
+      {tags.map((item) => {
+        const tagIsSelected = selectedTags?.includes(item.title);
 
-      <Box marginTop="10px">
-        {tags.map((item) => {
-          const tagIsSelected = selectedTags?.includes(item.title);
-
-          return (
-            <Tag
-              size="md"
-              key={item.id}
-              borderRadius="full"
-              variant={tagIsSelected ? "solid" : "outline"}
-              colorScheme="purple"
-              marginRight="10px"
-              marginTop="10px"
-              textTransform="capitalize"
-              onClick={() => handleSelection(item.title)}
-              cursor="pointer"
-            >
-              <TagLabel>{item.title}</TagLabel>
-              {tagIsSelected && <TagCloseButton />}
-            </Tag>
-          );
-        })}
-      </Box>
-    </Box>
+        return (
+          <Tag
+            size="md"
+            key={item.id}
+            borderRadius="full"
+            variant={tagIsSelected ? "solid" : "outline"}
+            colorScheme="purple"
+            marginRight="10px"
+            marginTop="10px"
+            textTransform="capitalize"
+            onClick={() => handleSelection(item.title)}
+            cursor="pointer"
+          >
+            <TagLabel>{item.title}</TagLabel>
+            {tagIsSelected && <TagCloseButton />}
+          </Tag>
+        );
+      })}
+    </>
   );
 }
