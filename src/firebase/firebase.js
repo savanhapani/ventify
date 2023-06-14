212dc0b4bca6b338166930a7eff4c42d288b37ad
Brijen Makwana <brijenmakwana@192.168.1.6>
2023-06-14 09:34:22 +0530

    prod and dev database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -20,38 +20,32 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 
-const firebaseConfigProd = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+const firebaseConfigEnv = {
+  prod: {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY_PROD,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PROD,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_PROD,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_PROD,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_PROD,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID_PROD,
+  },
+  dev: {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY_DEV,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_DEV,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_DEV,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_DEV,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_DEV,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID_DEV,
+  },
 };
 
-const firebaseConfigDev = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY_DEV,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_DEV,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_DEV,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_DEV,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_DEV,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID_DEV,
-};
-
-let firebaseConfig;
-
-if (import.meta.env.VITE_TESTING_ENVIRONMENT === "dev") {
-  firebaseConfig = firebaseConfigDev;
-} else {
-  firebaseConfig = firebaseConfigProd;
-}
+const firebaseConfig =
+  firebaseConfigEnv[import.meta.env.VITE_TESTING_ENVIRONMENT];
 
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
 export {
