a7a5280484c31bc5e1f5534d07a86984adaa47ce
Brijen Makwana <brijenmakwana@192.168.1.5>
2023-06-15 10:59:51 +0530

    Logo width

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,9 +7,9 @@ const Logo = () => {
       objectFit="contain"
       src={logo}
       alt="Ventify"
-      width="13vw"
+      width="14vw"
       minWidth="100px"
-      maxWidth="200px"
+      maxWidth="220px"
     />
   );
 };
