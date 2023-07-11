24c8fe1adcbd810bbc8b26cbff407c9e8e5f14c2
Brijen Makwana <brijenma@gmail.com>
2023-07-11 10:39:02 +0530

    AppliedFiltersHeading

--- src/pages/ConfessionsPage.jsx
+++ src/pages/ConfessionsPage.jsx
@@ -2,8 +2,6 @@ import {
   Flex,
   Box,
   useDisclosure,
-  Text,
-  Heading,
   Tabs,
   TabList,
   TabPanels,
@@ -35,7 +33,6 @@ import {
   increment,
   getDoc,
 } from "../firebase/firebase";
-import color from "../styles/colors";
 
 import AccountDrawer from "../components/AccountDrawer";
 import { VentifyContext } from "../context/VentifyContextProvider";
@@ -47,46 +44,7 @@ import VoteStatsModal from "../components/VoteStatsModal";
 import LoadindSpinner from "../components/LoadingSpinner";
 import { getConfessions } from "../helpers/posts/confessionHelpers";
 import ProtectedHeader from "../components/ProtectedHeader";
-
-const AppliedFiltersHeading = (props) => {
-  const { selectedCategories, selectedBatches } = props;
-
-  if (selectedCategories.length === 0 && selectedBatches.length === 0) {
-    return;
-  }
-
-  return (
-    <Heading
-      fontSize="21px"
-      paddingTop="15px"
-      paddingBottom="30px"
-      fontWeight="400"
-      textAlign="center"
-      marginRight="50px"
-      as="h2"
-    >
-      We are displaying confessions
-      {selectedCategories.length > 0 && " categorized under "}
-      <Text
-        as="span"
-        color={color.primary}
-        fontWeight="600"
-        textTransform="capitalize"
-      >
-        {selectedCategories.join(", ")}
-      </Text>
-      {selectedBatches.length > 0 && " from the batches "}
-      <Text
-        as="span"
-        color={color.primary}
-        fontWeight="600"
-        textTransform="capitalize"
-      >
-        {[...selectedBatches].sort((a, b) => a - b).join(", ")}
-      </Text>
-    </Heading>
-  );
-};
+import AppliedFiltersHeading from "../components/AppliedFiltersHeading";
 
 const ConfessionsPage = () => {
   const [confession, setConfession] = useState("");
