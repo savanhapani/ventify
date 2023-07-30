c1b24cf9b1de2d715e120f60d043742c80ab2c6a
Swarupa567 <swarupachowdaryp@gmail.com>
2023-07-30 22:02:11 +0530

    added highlight for hashtag

--- src/components/ConfessionText.jsx
+++ src/components/ConfessionText.jsx
@@ -1,8 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Highlight } from "@chakra-ui/react";
+import color from "../styles/colors";
 
 const ConfessionText = (props) => {
   const { confession } = props;
-  return <Text fontSize="lg">{confession}</Text>;
+  return <Text fontSize="lg">
+     <Highlight
+     query={['#CollegeLife', '#SleepyStudent', '#LibraryLurker']}
+     styles={{
+              px: "3",
+              py: "1",
+              color: color.primary,
+     }}>
+     {confession}
+     </Highlight>
+  </Text>;
 };
 
 export default ConfessionText;
